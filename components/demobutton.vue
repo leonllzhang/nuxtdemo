4ea786849cd9d7c2c9ec1323eeb3a5ea617bef0b
Leon LL Zhang <leon.ll.zhang@cn.pwc.com>
2021-05-08 09:31:34 +0800

    init

---
+++ components/demobutton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn @click="clickbtn">this is test button</v-btn>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        clickbtn: function () {
+            alert('clicked me!')
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
